175a24a084ec99ebc3785be3e85a3fab5cc190a0
Rajkumar Nagarajan <rajkumarnagarajan@Mac.fritz.box>
2026-01-25 18:45:29 +0100

    sdm120m - retry and max retries logic added

--- inc/sdm120m.h
+++ inc/sdm120m.h
@@ -11,9 +11,16 @@ public:
   const char* type() const override;
   bool read() override;
   float voltage() const;
+  // Optional tuning
+  void setRetries(uint8_t retries, uint16_t delayMs);
 
 private:
+  bool readOnce();
+
   uint8_t slave;
   ModbusBus& bus;
   float voltageValue;
+
+  uint8_t maxRetries = 3;
+  uint16_t retryDelayMs = 100;
 };
--- src/sdm120m.cpp
+++ src/sdm120m.cpp
@@ -3,15 +3,39 @@
 #include "../inc/logger.h"
 
 SDM120M::SDM120M(uint8_t id, ModbusBus& b)
-: slave(id), bus(b), voltageValue(0) {}
+: slave(id), bus(b), voltageValue(0.0f) {}
 
 const char* SDM120M::type() const {
   return "SDM120M";
 }
 
+void SDM120M::setRetries(uint8_t retries, uint16_t delayMs){
+    maxRetries = retries;
+    retryDelayMs = delayMs;
+}
+
 bool SDM120M::read() {
+  for (uint8_t attempt = 1; attempt <= maxRetries; attempt++) {
+    if (readOnce()) {
+      return true;
+    }
+
+    Logger::debug(
+      "SDM120M[%d] retry %d/%d",
+      slave,
+      attempt,
+      maxRetries
+    );
+
+    delay(retryDelayMs);
+  }
+
+  Logger::error("SDM120M[%d] read failed after %d retries", slave, maxRetries);
+  return false;
+}
+
+bool SDM120M::readOnce() {
   if (!bus.readInput(slave, 0x0000, 2)) {
-    Logger::error("SDM120M[%d] read failed", slave);
     return false;
   }
 
@@ -23,6 +47,7 @@ bool SDM120M::read() {
   memcpy(&voltageValue, &raw, sizeof(voltageValue));
   return true;
 }
+
 
 float SDM120M::voltage() const {
   return voltageValue;
